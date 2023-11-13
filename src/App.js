2e0d9df9515e3a20b529d7d94f1fc55d6a30e554
007-Shivam <shivambhosle270903@gmail.com>
2023-11-13 21:50:52 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/navbar/NavbarComponent';
+
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavbarComponent.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './NavbarComponent.css'
+
+export default function Navbar() {
+  return (
+    <header className="navbar_component">
+        <a href="/" className="logo">Biccas</a>
+        <nav>
+            <ul className="nav__links">
+                <li className="nav_list"><a className="navbar_links" href="/">Home</a></li>
+                <li className="nav_list"><a className="navbar_links" href="/">Product</a></li>
+                <li className="nav_list"><a className="navbar_links" href="/">FAQ</a></li>
+                <li className="nav_list"><a className="navbar_links" href="/">Blog</a></li>
+                <li className="nav_list"><a className="navbar_links" href="/">About Us</a></li>
+            </ul>
+        </nav>
+        <div>
+            <a href="/" className="login_button">Login</a>
+            <a href="/" className="cta"><button className="signup_button">Sign Up</button></a>
+        </div>
+
+    </header>
+  )
+}
